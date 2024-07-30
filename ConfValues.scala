e33086fc0b609b750de10a28797cf552730fa809
Artem Televnoy <117771505+ArtemTelevnoy@users.noreply.github.com>
2024-07-31 00:09:56 +0300

    Update ConfValues.scala

--- ConfValues.scala
+++ ConfValues.scala
@@ -1,5 +1,5 @@
 object ConfValues {
-  val token: String = "6882431264:AAG8wXBCQEOqLBmgIQ-SOEQ2U5ZFwm2iK-Q"
+  val token: String = "some token"
   val host: String = "localhost"
   val port: String = "5432"
   val baseName = "bot"
